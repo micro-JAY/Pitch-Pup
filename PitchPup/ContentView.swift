83d62be4044c7bf1cdce2b5db5633f70f856f7bc
micro-JAY <micro-JAY@users.noreply.github.com>
2026-02-01 15:55:05 +0900

    Initial Commit

---
+++ PitchPup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitchPup
+//
+//  Created by Jana Jennings on 2026/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitchPup/PitchPupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPupApp.swift
+//  PitchPup
+//
+//  Created by Jana Jennings on 2026/02/01.
+//
+
+import SwiftUI
+
+@main
+struct PitchPupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
